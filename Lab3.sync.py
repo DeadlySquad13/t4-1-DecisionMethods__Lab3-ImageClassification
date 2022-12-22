ad8d04be1400ad0eb83cb03301bfe168e5980d2d
DeadlySquad13 <46250621+DeadlySquad13@users.noreply.github.com>
2022-12-22 21:42:30 +0300

    [Enh](Model): Made model and exported it.

--- Lab3.sync.py
+++ Lab3.sync.py
@@ -152,8 +152,136 @@ train_masked_dataset = create_masked_dataset(train_subset, train_data, [transfor
 test_masked_dataset = create_masked_dataset(test_subset, test_data, [transform_label])
 
 # %% [markdown]
+# ## Создание DataLoader'а
+# ### Гиперпараметры
 
-# %% Create device
-device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+# %%
+LEARNING_RATE = 2.5e-3
+EPOCHS = 500
+
+data_loader_settings = {
+    'batch_size': 128,
+    'shuffle': True,
+}
+
+# %% [markdown]
+# ### Pytorch DataLoader
+
+# %%
+from torch.utils.data import DataLoader
+
+train_dataloader = DataLoader(train_masked_dataset, **data_loader_settings)
+
+test_dataloader = DataLoader(test_masked_dataset, **data_loader_settings)
+
+# %% [markdown]
+# ## Создание модели нейронной сети
+
+# %%
+import torch.nn as nn
+
+IMAGE_WITH = 32
+IMAGE_HEIGHT = 32
+IMAGE_COLORS = 3
+INPUT_SIZE = IMAGE_WITH * IMAGE_HEIGHT * IMAGE_COLORS
+
+class Cifar100NeutralNetwork(nn.Module):
+    def __init__(self, hidden_size=10, num_classes=3):
+        super(Cifar100NeutralNetwork, self).__init__()
+
+        self.flatten = nn.Flatten()
+        self.linear_relu_stack = nn.Sequential(
+            nn.Linear(INPUT_SIZE, hidden_size), # Сумматор.
+            nn.ReLU(), # Функция активации.
+            nn.Linear(hidden_size, num_classes)
+        )
+
+    def forward(self, x):
+        x = self.flatten(x)
+        logits = self.linear_relu_stack(x)
+
+        return logits
+
+# %%
+model = Cifar100NeutralNetwork(hidden_size=10, num_classes=len(LABELS))
+
+# %% [markdown]
+# ## Обучение модели
+# Прежде чем обучать модель, выберем функцию потерь и оптимизатор.
+
+# %%
+import torch.optim as optim
+
+loss_fn = nn.CrossEntropyLoss()
+
+optimizer = optim.SGD(model.parameters(), lr=LEARNING_RATE)
+
+
+# %%
+def train_loop(dataloader, model, loss_fn, optimizer):
+  """
+  Обучающий цикл оптимизации параметров
+  """
+  size = len(dataloader.dataset)
+  for batch, (X,y) in enumerate(dataloader):
+    # Расчёт предсказаний и ошибок
+    pred = model(X)
+    loss = loss_fn(pred, y)
+
+    # Обратное распространение ошибки
+    optimizer.zero_grad()
+    loss.backward()
+    optimizer.step()
+
+    if batch%5 == 0:
+      loss, current = loss.item(), batch*len(X)
+      print(f"loss: {loss:>7f}  [{current:>5d}/{size:>5d}]")
+
+def test_loop(dataloader, model, loss_fn):
+  """
+  Тестовый цикл оптимизации параметров
+  """
+  size = len(dataloader.dataset)
+  num_batches = len(dataloader)
+  test_loss, correct = 0, 0
+
+  with torch.no_grad():
+    for X, y in dataloader:
+      pred = model(X)
+      test_loss += loss_fn(pred, y).item()
+      correct += (pred.argmax(1) == y).type(torch.float).sum().item()
+
+  test_loss /= num_batches
+  correct /= size
+  print(f"Тестовая ошибка: \n Точность: {(100*correct):>0.1f}%, Avg loss: {test_loss:>8f} \n")
+
+for t in range(EPOCHS):
+    print(f"Эпоха {t+1}\n-------------------------------")
+    train_loop(train_dataloader, model, loss_fn, optimizer)
+    test_loop(test_dataloader, model, loss_fn)
+    print("Готово!")
+
+# %% [markdown]
+# ## Сохранение модели и экспорт в ONNX
+
+# %%
+MODEL_ROOT = 'model_result_data'
+MODEL_NAME = 'cifar100'
+
+torch.save(model, f'{MODEL_ROOT}/{MODEL_NAME}.pt')
+
+# Экспорт модели
+x = torch.randn(1, IMAGE_WITH, IMAGE_HEIGHT, IMAGE_COLORS, requires_grad=True).to('cpu')
+
+torch.onnx.export(model,               
+                  x,                   
+                  f'{MODEL_ROOT}/{MODEL_NAME}.onnx', 
+                  export_params=True,
+                  opset_version=9,     
+                  do_constant_folding=True,  
+                  input_names = ['input'],  
+                  output_names = ['output'],  
+                  dynamic_axes={'input' : {0 : 'batch_size'},    
+                                'output' : {0 : 'batch_size'}})
 
 
